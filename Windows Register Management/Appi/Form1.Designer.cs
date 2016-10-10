7bbd2f7e57887e05dc436b564a2f1fb7e414aed7
K <koskenniemi@gmail.com>
2016-10-10 13:03:52 +0300

    Windows Register Software

---
+++ Windows Register Management/Appi/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Appi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker4
+            // 
+            resources.ApplyResources(this.dateTimePicker4, "dateTimePicker4");
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.ValueChanged += new System.EventHandler(this.dateTimePicker4_ValueChanged);
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dateTimePicker4);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Windows Register Management/Appi/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+using Microsoft.Win32;
+using Microsoft.VisualBasic;
+
+namespace Appi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dateTimePicker4_ValueChanged(object sender, EventArgs e)
+        {
+
+            RegistryKey ke = Registry.LocalMachine;
+            RegistryKey key = ke.OpenSubKey("System\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\FirewallRules");
+            Array key2 = key.GetSubKeyNames();
+            Array name = key.GetValueNames();
+
+
+
+            dataGridView1.ColumnCount = 0;
+            DataGridViewCheckBoxColumn c = new DataGridViewCheckBoxColumn();
+            c.HeaderText = "Select";
+            dataGridView1.Columns.Add(c);
+            dataGridView1.Columns.Add("joo", "tieto 1");
+            dataGridView1.Columns.Add("joo2", "tieto 2");
+            dataGridView1.Columns.Add("joo3", "tieto 3");
+            dataGridView1.Columns.Add("joo4", "tieto 4");
+            dataGridView1.Columns.Add("joo5", "tieto 5");
+            dataGridView1.Columns.Add("joo6", "tieto 6");
+            dataGridView1.Columns.Add("joo7", "tieto 7");
+            dataGridView1.Columns.Add("joo8", "tieto 8");
+
+
+            for (int i=0; i<name.Length; i++)
+	       {
+               String j = (string)name.GetValue(i);
+               String k = (string)key.GetValue(j);
+
+                Array kk = k.Split('|');
+                Array k2 = (Array)k.Split('|');
+
+                dataGridView1.Rows.Add(false,
+                                        name.GetValue(i),
+                    
+                                        k2.GetValue(0),
+                                        k2.GetValue(1),
+                                        k2.GetValue(2), // 4
+                
+                                        k2.GetValue(3),
+                                        k2.GetValue(4),
+                
+                                        k2.GetValue(5),
+                                        k2.GetValue(6) // 8
+                      );
+
+	       }
+            
+            
+
+        }
+
+    }
+}
